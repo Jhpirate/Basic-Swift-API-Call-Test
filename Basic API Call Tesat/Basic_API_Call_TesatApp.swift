7b0114f20f54a7cdeda513d531d3717de8a5085e
Jared Heeringa <jaredheeringa@icloud.com>
2023-12-17 22:24:00 -0600

    Initial Commit

---
+++ Basic API Call Tesat/Basic_API_Call_TesatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_API_Call_TesatApp.swift
+//  Basic API Call Tesat
+//
+//  Created by Jared Heeringa on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Basic_API_Call_TesatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
